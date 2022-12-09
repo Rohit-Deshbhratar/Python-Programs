fda46f0116667430c4f5a9814c8b260aa0d596fc
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2022-12-09 12:36:52 +0530

    Count.py added.

---
+++ Python_String_Functions/Count.py
@@ -0,0 +1,9 @@
+# Study python string function endswith()
+# Date 9-DEC-2022
+# count() function counts number of occurences of any character, returns integer value
+# If substring not found returns zero
+
+string = 'apple'
+count = string.count('p')
+
+print(f"'p' occured in string '{string}' {count} times.")
