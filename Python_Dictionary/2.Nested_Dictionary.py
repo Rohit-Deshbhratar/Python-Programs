33fae90dfdde04b02d1ce5a80f6f45ef9df1c13b
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2023-02-18 10:52:27 +0530

    nested dictionary

---
+++ Python_Dictionary/2.Nested_Dictionary.py
@@ -0,0 +1,11 @@
+# Nested dictionary
+
+country = {'india' : {'states' : 'maharashtra', 'unionTerritory' : 'Pondicherry'},
+           'world' : {'continent' : 'Africa, America, Antarctica, Asia, Australia, Europe,', 
+                      'oceans' : 'Pacific, Arctic, Indian Ocean'}}
+print(country)
+
+# OR/Another way is, assign dictionary to another dictionary
+
+myDict = {'country' : country}
+print('\n', myDict)
