809b61d9b96f87b2794e08e88bacaf8a5919955b
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2023-02-18 10:40:26 +0530

    Basic of dictionary

---
+++ Python_Dictionary/1.Dictionay_basics.py
@@ -0,0 +1,19 @@
+# Basic structure of DICTIONARY using dict() constructor
+
+thisDict = dict(name = 'rohit', birthPlace = 'chandrapur', job = 'software tester')
+print(thisDict)
+
+# Basic format of DICTIONARY
+
+carInfo = {'brand' : 'Ford', 'model' : 'Mustang', 'Year' : 1964}
+print(carInfo)
+
+# Changing values in DICTIONARY
+
+carInfo['Year'] = 2023
+print(carInfo)
+
+# Adding an item into DICTIONARY
+
+carInfo['color'] = 'red'
+print(carInfo)
