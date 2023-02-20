ca0f1cad91c6b00fc3017d7de5d47ed8253e0a51
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2023-02-20 10:44:37 +0530

    Set basics

---
+++ Python_Sets/1.Set_basics.py
@@ -0,0 +1,12 @@
+# Empty set
+setS = set()
+print(type(setS))
+
+# Creating a set
+setS = {1, 2, 3}
+print(f"Type: {type(setS)}, Values: {setS}")
+
+# Check accept duplicate value or not
+
+setS = {1, 2, 2, 3, 5, 5, 6, 8}
+print(f"Values: {setS}, Length: {len(setS)}")
