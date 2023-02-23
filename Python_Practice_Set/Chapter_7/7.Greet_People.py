ece3b7f485c3a4a2b89c600b928eedccc45ab94c
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2023-02-23 12:12:48 +0530

    Greet

---
+++ Python_Practice_Set/Chapter_7/7.Greet_People.py
@@ -0,0 +1,10 @@
+# Write a program to greet all the person names stored in a list l1 and which starts with S.
+# l1 = ['Harry', 'Sohan', 'Sachin', 'Rahul']
+
+l1 = ['Harry', 'Sohan', 'Sachin', 'Rahul']
+
+for i in l1:
+    if (i.startswith('S')):
+        print(f"Hello {i}")
+    else:
+        pass
