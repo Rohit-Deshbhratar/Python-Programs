afc2a0804eeb2ae66a93de1f543bb895f677bfbe
Rohit-Deshbhratar <deshbhratarrohit9@gmail.com>
2023-05-24 14:12:33 +0530

    2D Array basics

---
+++ Python_Arrays/5.2D_Array.py
@@ -0,0 +1,33 @@
+# Creating 2D array
+
+import array
+
+td_array = [[2, 5], [6, 9]]
+
+# Printing two dimentional array
+print("Printing two dimentional array...")
+print(td_array)
+
+# First row
+print("\nPrinting first row...")
+print(td_array[0])
+
+# Second row
+print("\nPrinting second row...")
+print(td_array[1])
+
+# First row first column
+print("\nPrinting item at first row first column...")
+print(td_array[0][0])
+
+# First row second column
+print("\nPrinting item at first row second column...")
+print(td_array[0][1])
+
+# Second row first column
+print("\nPrinting item at second row first column...")
+print(td_array[1][0])
+
+# Second row second column
+print("\nPrinting item at second row second column...")
+print(td_array[1][1])
